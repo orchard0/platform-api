d1f1f977d94978a3e087db4c89c60306b9306329
arl <arlatif@pm.me>
2024-10-16 11:21:11 +0100

    add url in start up console log

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const app = require('./server/index');
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () =>
-	console.log(`Bismillah ar-Rahman ar-Raheem. Listening on ${PORT}...`)
+	console.log(
+		`Bismillah ar-Rahman ar-Raheem. Listening on http://localhost:${PORT}`
+	)
 );
